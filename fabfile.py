116c2d9edbabf7a041b52811d3614d63f5acab39
Ben Keith <ben@benlk.com>
2016-11-23 16:15:03 -0500

    Fix staging user

--- fabfile.py
+++ fabfile.py
@@ -45,7 +45,7 @@ def staging():
     """
     env.settings    = 'staging'
     env.hosts       = [ os.environ[ 'VOICEOFOC_STAGING_SFTP_HOST' ], ]   # ssh host for production.
-    env.user        = os.environ[ 'VOICEOFOC_STAGING_SFTP_USER' ],       # ssh user for production.
+    env.user        = os.environ[ 'VOICEOFOC_STAGING_SFTP_USER'  ]       # ssh user for production.
     env.password    = os.environ[ 'VOICEOFOC_STAGING_SFTP_PASSWORD' ]    # ssh password for production.
     env.domain      = 'voiceofoc.staging.wpengine.com'
     env.port        = '2222'
