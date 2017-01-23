764b2ddd9af131c2243690bd1ec2a9ebb455b8a8
aschweigert <aschweigert@gmail.com>
2017-01-23 14:25:18 -0500

    add member constants to child theme

--- wp-content/themes/voiceofoc/functions.php
+++ wp-content/themes/voiceofoc/functions.php
@@ -1,5 +1,14 @@
 <?php
 
+// This site is an INN Member
+if ( ! defined( 'INN_MEMBER' ) ) {
+        define( 'INN_MEMBER', true );
+}
+// This site is hosted by INN
+if ( ! defined( 'INN_HOSTED' ) ) {
+        define( 'INN_HOSTED', true );
+}
+
 /**
  * Includes
  */
