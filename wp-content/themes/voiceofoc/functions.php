f0ba94377fdc267d8c21312fb96eb32b8958e171
Ben Keith <ben@benlk.com>
2017-03-13 12:25:56 -0400

    Implement http://anantgarg.com/2016/06/22/typekit-fout-fix/ for typekit fonts

--- wp-content/themes/voiceofoc/functions.php
+++ wp-content/themes/voiceofoc/functions.php
@@ -48,17 +48,50 @@ add_action( 'wp_enqueue_scripts', 'voiceofoc_stylesheet', 20 );
 
 /**
  * Typekit fonts
+ * Because of a persistant Flash of Uninitialized Text on the part of Voice of OC staff, we're going to implement http://anantgarg.com/2016/06/22/typekit-fout-fix/
+ * @since 2017-03-13
  */
 function voiceofoc_typekit() {
+		// normal Typekit script:
+		// <script src="https://use.typekit.net/xyq0vny.js"></script>
+		// <script>try{Typekit.load({ async: true });}catch(e){}</script>
 	?>
-		<script src="https://use.typekit.net/xyq0vny.js"></script>
-		<script>try{Typekit.load({ async: true });}catch(e){}</script>
+		<script>
+			(function(d) {
+				loadFonts = 1;
+				if(window.sessionStorage){
+					if(sessionStorage.getItem('useTypekit')==='false'){
+						loadFonts = 0;
+					}
+				}
+				if (loadFonts == 1) {
+						var config = {
+							kitId: 'xyq0vny',
+							scriptTimeout: 1000,
+							async: true
+						},
+						h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";if(window.sessionStorage){sessionStorage.setItem("useTypekit","false")}},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+="wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s);
+					}
+				}
+			)(document);
+		</script>
+		<style>
+			.wf-loading p, .wf-loading h1, .wf-loading h2, .wf-loading h3, .wf-loading h4 {
+				visibility: hidden;
+			}
+			.wf-active p, .wf-active h1, .wf-active h2, .wf-active h3, .wf-active h4 {
+				visibility: visible;
+			}
+			.wf-inactive p, .wf-inactive h1, .wf-inactive h2, .wf-inactive h3, .wf-inactive h4 {
+				visibility: visible;
+			}
+		</style>
 	<?php
 }
 add_action( 'wp_head', 'voiceofoc_typekit' );
 
 /**
- * Added by reques
+ * Added by request
  * @since Largo 0.5.5.3
  * @since February 20, 2017
  */
