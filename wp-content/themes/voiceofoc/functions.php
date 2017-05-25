7d5227fdfbd362d8ef12b1b5353081ae50bdaf79
Ben Keith <ben@benlk.com>
2017-05-24 22:35:21 -0400

    Change the width at which the header subscribe buttons are hidden, to prevent overflow on phones

--- wp-content/themes/voiceofoc/functions.php
+++ wp-content/themes/voiceofoc/functions.php
@@ -2,11 +2,11 @@
 
 // This site is an INN Member
 if ( ! defined( 'INN_MEMBER' ) ) {
-		define( 'INN_MEMBER', true );
+	define( 'INN_MEMBER', true );
 }
 // This site is hosted by INN
 if ( ! defined( 'INN_HOSTED' ) ) {
-		define( 'INN_HOSTED', true );
+	define( 'INN_HOSTED', true );
 }
 
 /**
