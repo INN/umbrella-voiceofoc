ee5e1be60e387972564deac76e9ef3dcfb8a05aa
Ben Keith <ben@benlk.com>
2017-08-31 15:39:37 -0400

    Responsive styles for the widgets
    - between 769 and 979px, 4-col drops to 2-col
    - below 768px, 4-, 3-, 2-col all drop to 1-col

--- wp-content/themes/voiceofoc/partials/widget-header-area.php
+++ wp-content/themes/voiceofoc/partials/widget-header-area.php
@@ -1,7 +1,7 @@
 <?php 
 
 if ( voiceofoc_widget_header_area_enabled() ) {
-	echo '<div class="row">';
+	echo '<div class="row" id="widget-header-area">';
 		dynamic_sidebar( 'widget-header-area' );
 	echo '</div>';
 }
