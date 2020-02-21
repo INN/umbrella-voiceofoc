7429dc94a585944b2635a8b7f218446bcd878147
Ben Keith <ben@benlk.com>
2020-02-21 14:44:08 -0500

    remove header-search in nav-main

--- wp-content/themes/voiceofoc/partials/nav-main.php
+++ wp-content/themes/voiceofoc/partials/nav-main.php
@@ -50,15 +50,6 @@ if ( is_front_page() || is_home() || is_archive() ): ?>
 					</li>
 				<?php } ?>
 			</ul>
-			<?php if ( is_home() || is_front_page() ) { ?>
-				<div id="header-search">
-					<form class="form-search" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-						<div class="input-append">
-							<input type="text" placeholder="<?php _e('Search', 'largo'); ?>" class="input-medium appendedInputButton search-query" value="" name="s" /><button type="submit" class="search-submit btn"><i class="icon-search"></i></button>
-						</div>
-					</form>
-				</div>
-			<?php } ?>
 		</div>
 	</div>
   </div>
